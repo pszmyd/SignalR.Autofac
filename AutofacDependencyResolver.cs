770eefd214e840c8cf6969afef54c3f834540715
Sam Owens <sam.owens@easyjet.com>
2012-06-28 09:59:24 +0100

    Changed resolver to register itsef as an IRegistrationSource in Autofac

--- AutofacDependencyResolver.cs
+++ AutofacDependencyResolver.cs
@@ -1,106 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using Autofac.Core;
+using Autofac.Core.Activators.ProvidedInstance;
+using Autofac.Core.Lifetime;
+using Autofac.Core.Registration;
 
 namespace SignalR.Autofac
 {
-    using System.Linq;
-    using System.Linq.Expressions;
-    using System.Reflection;
-
-    using global::Autofac;
+	using global::Autofac;
 
     /// <summary>
     /// SingalR dependency resolver using Autofac container as backend.
     /// </summary>
-    public class AutofacDependencyResolver : DefaultDependencyResolver
+    public class AutofacDependencyResolver : DefaultDependencyResolver, IRegistrationSource
     {
-        private ContainerBuilder _builder = new ContainerBuilder();
-        private readonly IDictionary<Type, Func<object>> _registrations = new Dictionary<Type, Func<object>>();
+		private readonly ILifetimeScope _lifetimeScope;
 
-        private bool dirty = true;
+		public AutofacDependencyResolver(ILifetimeScope lifetimeScope)
+		{
+			_lifetimeScope = lifetimeScope;
+			_lifetimeScope.ComponentRegistry.AddRegistrationSource(this);
+		}
 
-        public AutofacDependencyResolver(ILifetimeScope scope)
-        {
-            Scope = scope;
-            FlushRegistryCache();
-        }
+		public override object GetService(Type serviceType)
+		{
+			object result;
+			_lifetimeScope.TryResolve(serviceType, out result);
+			return result;
+		}
 
-        public ILifetimeScope Scope { get; set; }
+		public override IEnumerable<object> GetServices(Type serviceType)
+		{
+			object result;
+			_lifetimeScope.TryResolve(serviceType.MakeArrayType(), out result);
+			return (IEnumerable<object>)result;
+		}
 
-        private void FlushRegistryCache()
-        {
-            _builder = new ContainerBuilder();
-            foreach (var kv in _registrations)
-            {
-                RegisterInContainer(_builder, kv.Key, kv.Value, Scope);
-            }
-            _builder.Update(Scope.ComponentRegistry);
-            _registrations.Clear();
-            dirty = false;
-        }
+		IEnumerable<IComponentRegistration> IRegistrationSource.RegistrationsFor(Service service, Func<Service, IEnumerable<IComponentRegistration>> registrationAccessor)
+		{
 
-        public override void Register(Type serviceType, Func<object> activator)
-        {
-            if (Scope == null)
-                _registrations.Add(serviceType, activator);
-            else
-            {
-                if(dirty) this.FlushRegistryCache();
-                _builder = new ContainerBuilder();
-                RegisterInContainer(_builder, serviceType, activator, Scope);
-                _builder.Update(Scope.ComponentRegistry);
-            }
+			var result = new IComponentRegistration[] { };
+			var serviceType = service as TypedService;
+			if (serviceType != null)
+			{
+				var instance = base.GetService(serviceType.ServiceType);
+				if (instance != null)
+				{					
+					result = new IComponentRegistration[]
+					         	{
+					         		new ComponentRegistration(
+					         			Guid.NewGuid(),
+					         			new ProvidedInstanceActivator(instance),
+					         			new CurrentScopeLifetime(), 
+					         			InstanceSharing.Shared,
+					         			InstanceOwnership.ExternallyOwned,
+					         			new[] {service},
+					         			new Dictionary<string, object>())
+					         	};
+				}				
+			}
 
-        }
+			return result;
+		}
 
-        public override void Register(Type serviceType, IEnumerable<Func<object>> activators)
-        {
-            if (Scope == null)
-                _registrations.Add(serviceType, activators.Last());
-            else
-            {
-                if (dirty) this.FlushRegistryCache();
-                _builder = new ContainerBuilder();
-                foreach (var activator in activators)
-                {
-                    RegisterInContainer(_builder, serviceType, activator, Scope);
-                }
-                _builder.Update(Scope.ComponentRegistry);
-            }
-        }
-
-        public override object GetService(Type serviceType)
-        {
-            Func<object> factory;
-            if (Scope == null) return _registrations.TryGetValue(serviceType, out factory) ? factory() : null;
-
-            if (dirty) this.FlushRegistryCache();
-            object instance;
-            return Scope.TryResolve(serviceType, out instance) ? instance : null;
-        }
-
-        public override IEnumerable<object> GetServices(Type serviceType)
-        {
-            Func<object> factory;
-            if (Scope == null) return _registrations.TryGetValue(serviceType, out factory) ? new List<object> { factory() } : null;
-
-            if (dirty) this.FlushRegistryCache();
-            object instance;
-            return Scope.TryResolve(typeof(IEnumerable<>).MakeGenericType(serviceType), out instance) ? instance as IEnumerable<object> : null;
-        }
-
-        private static void RegisterGeneric<T>(ContainerBuilder builder, Type type, Func<object> obj, ILifetimeScope scope)
-        {
-            builder.Register(context => (T)obj()).As(type).PreserveExistingDefaults().
-                InstancePerLifetimeScope().
-                InstancePerMatchingLifetimeScope("shell");
-        }
-
-        private static void RegisterInContainer(ContainerBuilder builder, Type type, Func<object> obj, ILifetimeScope scope)
-        {
-            var method = typeof(AutofacDependencyResolver).GetMethod("RegisterGeneric", BindingFlags.Static | BindingFlags.NonPublic).MakeGenericMethod(type);
-            var delegateType = Expression.GetActionType(method.GetParameters().Select(p => p.ParameterType).ToArray());
-            Delegate.CreateDelegate(delegateType, method).DynamicInvoke(builder, type, obj, scope);
-        }
+		bool IRegistrationSource.IsAdapterForIndividualComponents
+		{
+			get { return false; }
+		}
     }
 }
