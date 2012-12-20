6b6f11cb4f9b1c87d03447b98c39fd194546fcf3
Piotr Szmyd <piotr.szmyd@gmail.com>
2012-12-20 06:30:14 +0100

    Added some initial UTs

---
+++ Tests/AutofacTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Autofac.Builder;
+using Autofac.Core;
+using Autofac.Features.Indexed;
+using Microsoft.AspNet.SignalR;
+using NUnit.Framework;
+
+namespace SignalR.Autofac.Tests
+{
+    [TestFixture]
+    public class AutofacTests
+    {
+        [Test]
+        public void DependenciesShouldGetResolvedInNamedScope()
+        {
+            var rootBuilder = new ContainerBuilder();
+            rootBuilder.RegisterDependencyResolver().InstancePerMatchingLifetimeScope("shell");
+            var rootContainer = rootBuilder.Build();
+
+            var scope = rootContainer.BeginLifetimeScope("shell");
+            var connMgr = scope.Resolve<IConnectionManager>();
+
+            Assert.That(connMgr, Is.Not.Null);
+
+            var container = scope.Resolve<IDependencyResolver>();
+
+            Assert.That(container, Is.Not.Null);
+        }
+
+        [Test]
+        public void DependenciesShouldGetResolvedWhenRegisteredFromModule()
+        {
+            var rootBuilder = new ContainerBuilder();
+            var rootContainer = rootBuilder.Build();
+            var intermediateScope = rootContainer.BeginLifetimeScope(builder => builder
+                .RegisterType(typeof(TestModule))
+                .Keyed<IModule>(typeof(TestModule))
+                .InstancePerDependency());
+
+            var scope = intermediateScope.BeginLifetimeScope("shell", builder =>
+            {
+                var moduleIndex = intermediateScope.Resolve<IIndex<Type, IModule>>();
+                builder.RegisterModule(moduleIndex[typeof (TestModule)]);
+            });
+
+            var connMgr = scope.Resolve<IConnectionManager>();
+
+            Assert.That(connMgr, Is.Not.Null);
+
+            var container = scope.Resolve<IDependencyResolver>();
+
+            Assert.That(container, Is.Not.Null);
+        }
+    }
+}
---
+++ Tests/TestModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace SignalR.Autofac.Tests
+{
+    public class TestModule : Module
+    {
+        protected override void Load(ContainerBuilder moduleBuilder)
+        {
+            moduleBuilder.RegisterDependencyResolver().InstancePerMatchingLifetimeScope("shell");
+        }
+    }
+}
